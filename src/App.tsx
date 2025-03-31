dc7347b80fbc33643effbee53c949f736ef35a07
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2025-03-31 23:24:30 +0700

    init react-aria-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from 'react-aria-components';
+
 export function App() {
   return (
     <>
       <h1 className="text-4xl font-semibold">Hi! From Yumeno</h1>
+      <Button className="rounded bg-blue-500 px-3 py-2 text-white hover:bg-blue-600">React Aria Button</Button>
     </>
   );
 }
